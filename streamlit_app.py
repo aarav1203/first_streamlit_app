e48210e2ae4ebdf7cbd0108359745a64b0867aaa
AARAV KUMAR SINGH <62836744+aarav1203@users.noreply.github.com>
2023-04-12 18:25:33 +0530

    Update streamlit_app.py
    header and some text updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title("This is first line of this app")
+streamlit.title("My parents new healthy dinner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Bluberry OatMeal")
+streamlit.text("Kale and spinach")
+streamlit.text("Handboilded free egg")
